b651708afe700d70cce5499a35c44852df3d0541
asil5 <asiltokac2@gmail.com>
2026-06-06 00:28:34 +0100

    fix: whatsapp number updated to mobile

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 export const site = {
   phone: "020 8058 6674",
-  whatsapp: "+442080586674",
+  whatsapp: "+447946057224",
   email: "info@dumbwaiter.uk",
   company: "Dumbwaiter UK",
   tagline: "London's Dumbwaiter Specialist",
